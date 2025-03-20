f57fd32e5edbf53deddf9349c8dd0b40255e8e20
gabe-green <johngabrielgreen@gmail.com>
2025-03-20 11:02:42 -0600

    Update Labor Hours by Phase.sql
    Changing order of tables and simplifying to get phases without transactions to persist

--- Labor Hours by Phase.sql
+++ Labor Hours by Phase.sql
@@ -1,46 +1,23 @@
-/*
-	Title: Labor Hours by Phase Technology Solutions
-	Purpose: Create a crystal report that references this view.  Shows projected hours, Estimated, hours, WTD, JTD, and Percent complete for previouse month.   
-	Author: Gabe Green
-	Created: 3/20/25
-	Last Edited: 3/20/25
-	Change Log: 
-*/
-
-
-SELECT 
+SELECT
 	phases.Job_Number
-	--,jobs.Job_Description
+	,jobs.Job_Description
 	,phases.Phase_Code
-	,jcthist.Cost_Type
-	,SUM(jcthist.Total_Hours) Hours
-	--,CAST(Tran_Date_Text AS DATE) Tran_Date 
-	--,CAST(GETDATE() as Date) Today
-	--,DATEDIFF(DAY, CAST(Tran_Date_Text AS DATE), CAST(GETDATE() as Date)) Date_Diff
-	--TOP 1000 * 
-FROM 
-	
-	
-	JC_PHASE_MASTER_MC phases 
-	FULL OUTER JOIN JC_TRANSACTION_HISTORY_MC jcthist ON phases.Job_Number = jcthist.Job_Number and phases.Phase_Code = jcthist.Phase_Code
---	LEFT OUTER JOIN JC_JOB_MASTER_MC jobs ON jcthist.Job_Number = jobs.Job_Number
-	
-	
+	,ISNULL(sum(trans.Total_Hours),0) Hours
 
-
+FROM
+	JC_PHASE_MASTER_MC phases
+	LEFT OUTER JOIN JC_TRANSACTION_HISTORY_MC trans 
+		on phases.job_number = trans.job_number 
+		and phases.Phase_Code = trans.Phase_Code
+	LEFT OUTER JOIN JC_JOB_MASTER_MC jobs
+		on jobs.Job_Number = phases.Job_Number
 
 WHERE 
-	ltrim(jcthist.Job_Number) like '25%'
-	and jcthist.Cost_Center in ('130','140','150')
-	and jcthist.Cost_Type in ('L','V','C')
-	--and DATEDIFF(DAY, CAST(Tran_Date_Text AS DATE), CAST(GETDATE() as Date)) < 30
-	--and SUM(Total_Hours) = 0
-GROUP BY 
-	phases.Job_Number
-	--,jobs.Job_Description
+	LTRIM(phases.Job_Number) like '25602'
+GROUP BY
+	phases.job_number
+	,jobs.Job_Description
 	,phases.Phase_Code
-	,jcthist.Cost_Type
-	--,Tran_Date_Text
+	--,trans.Total_Hours 
 ORDER BY 
-	Job_Number,Phase_Code;
-	
+	phases.Phase_Code
