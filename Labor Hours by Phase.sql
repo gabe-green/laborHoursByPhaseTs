b96f866c2c89022e82afe7bcbcf86eccfc5a524e
gabe-green <johngabrielgreen@gmail.com>
2025-03-20 08:10:31 -0600

    Update Labor Hours by Phase.sql
    Added Cost type

--- Labor Hours by Phase.sql
+++ Labor Hours by Phase.sql
@@ -1,20 +1,24 @@
 SELECT 
 	Job_Number
 	,Phase_Code
+	,Cost_Type
 	,SUM(Total_Hours) Hours
 	,CAST(Tran_Date_Text AS DATE) Tran_Date 
 	--,CAST(GETDATE() as Date) Today
 	,DATEDIFF(DAY, CAST(Tran_Date_Text AS DATE), CAST(GETDATE() as Date)) Date_Diff
 	--TOP 1000 * 
 FROM JC_TRANSACTION_HISTORY_MC
+
 WHERE 
 	ltrim(Job_Number) like '25607'
 	and Cost_Center in (/*'130','140',*/'150')
 	and Cost_Type in ('L','V','C')
-	--and DATEDIFF(DAY, CAST(Tran_Date_Text AS DATE), CAST(GETDATE() as Date)) < 30	
+	and DATEDIFF(DAY, CAST(Tran_Date_Text AS DATE), CAST(GETDATE() as Date)) < 30
+	and Cost_Type = 'L'
 GROUP BY 
 	Job_Number
 	,Phase_Code
+	,Cost_Type
 	,Tran_Date_Text
 ORDER BY Tran_Date_Text
 	
